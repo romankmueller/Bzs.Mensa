dfc4e4322edcb25ac4759d60a51a0bd8cb41bfa3
Roman Mueller <roman.mueller@deltaworks.ch>
2022-02-24 14:32:50 +0100

    log4net

--- BZS Mensa/Bzs.Mensa.Server.Services/ServiceBase.cs
+++ BZS Mensa/Bzs.Mensa.Server.Services/ServiceBase.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
-using System.Configuration;
+﻿using System.Configuration;
+using System.Reflection;
+using Bzs.Mensa.Server.DataAccess.Context;
+using log4net;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Bzs.Mensa.Server.Services
 {
@@ -8,7 +12,9 @@ namespace Bzs.Mensa.Server.Services
     /// </summary>
     public abstract class ServiceBase
     {
-        //private readonly DbContextOptionsBuilder<BzsMensaContext> optionsBuilder;
+        protected static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private readonly DbContextOptionsBuilder<BzsMensaContext> optionsBuilder;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ServiceBase" /> class.
@@ -19,20 +25,116 @@ namespace Bzs.Mensa.Server.Services
             string connectionString = configuration.GetConnectionString(@"BzsMensa");
             if (connectionString == null)
             {
-                throw new ConfigurationErrorsException("There is no connection string 'BzsMensa' configured.");
+                this.LogError(@"There is no connection string 'BzsMensa' configured.");
+                throw new ConfigurationErrorsException(@"There is no connection string 'BzsMensa' configured.");
             }
 
-            //this.optionsBuilder = new DbContextOptionsBuilder<BzsMensaContext>();
-            //this.optionsBuilder.UseSqlServer(connectionString);
+            this.optionsBuilder = new DbContextOptionsBuilder<BzsMensaContext>();
+            this.optionsBuilder.UseSqlServer(connectionString);
         }
 
         /// <summary>
         /// Creates an entity framework context.
         /// </summary>
         /// <returns>The context.</returns>
-        //protected BzsMensaContext CreateContext()
-        //{
-        //    return new BzsMensaContext(this.optionsBuilder.Options);
-        //}
+        protected BzsMensaContext CreateContext()
+        {
+            return new BzsMensaContext(this.optionsBuilder.Options);
+        }
+
+        /// <summary>
+        /// Writes an info log entry.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        protected void LogInfo(object message)
+        {
+            Log.Info(message);
+        }
+
+        /// <summary>
+        /// Writes an info log entry.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="exception">The exception.</param>
+        protected void LogInfo(object message, Exception exception)
+        {
+            Log.Info(message, exception);
+        }
+
+        /// <summary>
+        /// Writes a debug log entry.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        protected void LogDebug(object message)
+        {
+            Log.Debug(message);
+        }
+
+        /// <summary>
+        /// Writes a debug log entry.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="exception">The exception.</param>
+        protected void LogDebug(object message, Exception exception)
+        {
+            Log.Debug(message, exception);
+        }
+
+        /// <summary>
+        /// Writes an warning log entry.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        protected void LogWarn(object message)
+        {
+            Log.Warn(message);
+        }
+
+        /// <summary>
+        /// Writes an warn log entry.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="exception">The exception.</param>
+        protected void LogWarn(object message, Exception exception)
+        {
+            Log.Warn(message, exception);
+        }
+
+        /// <summary>
+        /// Writes an error log entry.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        protected void LogError(object message)
+        {
+            Log.Error(message);
+        }
+
+        /// <summary>
+        /// Writes an error log entry.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="exception">The exception.</param>
+        protected void LogError(object message, Exception exception)
+        {
+            Log.Error(message, exception);
+        }
+
+        /// <summary>
+        /// Writes a fatal log entry.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        protected void LogFatal(object message)
+        {
+            Log.Fatal(message);
+        }
+
+        /// <summary>
+        /// Writes a fatal log entry.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="exception">The exception.</param>
+        protected void LogFatal(object message, Exception exception)
+        {
+            Log.Fatal(message, exception);
+        }
     }
 }
---
+++ BZS Mensa/Bzs.Mensa.Server.UnitTest/AssemblyUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bzs.Mensa.Server.UnitTest
+{
+    /// <summary>
+    /// Represents the assembly unit test.
+    /// </summary>
+    [TestClass]
+    public sealed class AssemblyUnitTest
+    {
+        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// Initializes the test assembly.
+        /// </summary>
+        /// <param name="context">The test context.</param>
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            XmlConfigurator.Configure(new FileInfo("log4net.config"));
+        }
+
+        /// <summary>
+        /// Tests the log4net configuration.
+        /// </summary>
+        [TestMethod]
+        public void Log4NetConfigured()
+        {
+            Log.Info("log4net configured.");
+        }
+    }
+}
