6d659e990ec514446cbc6a5d534b247cff915601
Roman Mueller <roman.mueller@deltaworks.ch>
2022-02-24 15:11:19 +0100

    Web Projekt

---
+++ BZS Mensa/Bzs.Mensa.Server.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bzs.Mensa.Server.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
