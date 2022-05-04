d880dc0f4bef87c934716d63dc44801dce90e844
Roman Mueller <roman.mueller@deltaworks.ch>
2022-05-04 20:49:26 +0200

    Passwort

---
+++ BZS Mensa/Bzs.Mensa.Shared/DataTransferObjects/BenutzerPasswortVergessenDto.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Bzs.Mensa.Shared.DataTransferObjects
+{
+    /// <summary>
+    /// Represents a user password forgotten data transfer object.
+    /// </summary>
+    [JsonObject]
+    public sealed class BenutzerPasswortVergessenDto : DtoBase
+    {
+    }
+}
